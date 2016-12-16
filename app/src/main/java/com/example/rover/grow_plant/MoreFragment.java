7ce65d97b17e567068f7a6c3a25c2f6e3f69ddac
Rover <rover@RoverdeMacBook-Pro.local>
2016-12-16 10:49:13 +0800

    调通Tcp发送接收

--- app/src/main/java/com/example/rover/grow_plant/MoreFragment.java
+++ app/src/main/java/com/example/rover/grow_plant/MoreFragment.java
@@ -1,23 +1,92 @@
 package com.example.rover.grow_plant;
 
 import android.app.Fragment;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import java.io.IOException;
 
 /**
  * Created by rover on 2016/12/7.
  */
 
-public class MoreFragment extends Fragment{
+public class MoreFragment extends Fragment implements RoverTCPSocket.ConnectListener{
+
+
+    private RoverTCPSocket tcpSocket;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
 
-        return inflater.inflate(R.layout.more_fragment, container, false);
+        View view = inflater.inflate(R.layout.more_fragment, container, false);
+
+        ImageButton btn = (ImageButton)view.findViewById(R.id.connectBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tcpSocket == null){
+                    RoverTCPSocket.ConnectListener lcon = new RoverTCPSocket.ConnectListener() {
+                        @Override
+                        public void onReceiveData(String data) {
+                            Log.i("TCP主机返回的数据"," = "+data);
+                        }
+
+                        @Override
+                        public void connected() {
+                            Log.i("TCP主机返回的数据"," 连接成功   123");
+                        }
+                    };
+                    tcpSocket = new RoverTCPSocket(lcon);
+                    tcpSocket.host = "192.168.1.183";
+                    tcpSocket.hostPort = 5000;
+                    try {
+                        tcpSocket.connectToHost();
+                        Log.i("connect","here");
+                    }catch (IOException ex){
+                        Log.i("异常 ","ex = ",ex);
+                    }
+                }
+            }
+        });
+
+
+
+        ImageButton sendBtn = (ImageButton)view.findViewById(R.id.sendDataBtn);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    tcpSocket.sendData("fuck");
+                    Log.i("发送数据","发送数据");
+                }catch (IOException io){
+                    Log.i("发送数据异常","don't do this");
+                }
+
+            }
+        });
+
+
+        return view;
+    }
+
+
+    public void onReceiveData(String data){
+
+
+        Log.i("TCP主机返回的数据"," = "+data);
+
+    }
+
+    public void connected(){
+        Log.i("TCP主机返回的数据"," 连接成功  000");
     }
 
 
--- app/src/main/java/com/example/rover/grow_plant/RoverTCPSocket.java
+++ app/src/main/java/com/example/rover/grow_plant/RoverTCPSocket.java
@@ -1,26 +1,150 @@
 package com.example.rover.grow_plant;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
 import java.net.Socket;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.SocketAddress;
+import java.net.InetSocketAddress;
 
 /**
  * Created by rover on 2016/12/14.
  */
 
-public class RoverTCPSocket {
+public class RoverTCPSocket implements Runnable{
 
 
     private Socket socket;
 
-    private String host;
+    public String host;
 
-    private int    hostPort;
+    public int    hostPort;
+
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private InputStream inputStream;
+    private OutputStream outputStream;
 
 
-    //连接主机
-    public void connectToHost(){
+    private ConnectListener mListener;
+
+    public interface ConnectListener{
+        void onReceiveData(String data);
+        void connected();
+    }
+
+
+    public void run() {
+
+    }
+
+
+    public RoverTCPSocket(ConnectListener callback) {
+        this.mListener = callback;
+    }
+
+
+    public void connectToHost ()throws IOException
+    {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (socket == null) {
+                        socket = new Socket();
+
+                        SocketAddress address = new InetSocketAddress(host, hostPort);
+                        socket.connect(address, 1000);// 连接指定IP和端口
+
+                        if (socket.isConnected()) {
+                            inputStream = socket.getInputStream();
+                            outputStream = socket.getOutputStream();
+                            mListener.connected();
+                        }
+
+                    }
+                    Log.i("connect", "here");
+                } catch (IOException ex) {
+                    Log.i("异常 ", "ex = ", ex);
+                }
+
+
+                Log.i("连接socket", "结果" + socket);
+
+
+                try {
+                    InputStream inputStream = socket.getInputStream();
+                    byte[] buffer = new byte[1024];
+                    int len = -1;
+
+                    while ((len = inputStream.read(buffer)) != -1) {
+
+                        String data = new String(buffer, 0, len);
+                        //通过回调接口将获取到的数据推送出去
+                        if (mListener != null) {
+                            mListener.onReceiveData(data);
+                        }
+                    }
+                }catch (IOException ex) {
+                    Log.i("异常 ", "ex = ", ex);
+                }
+            }
+        }).start();
+    }
 
 
 
+    /**
+     * 返回连接服是否成功
+     *
+     * @return
+     */
+    public boolean isConnected_rover() {
+        if(socket != null){
+
+            Log.i("检测","返回连接服是否成功");
+
+            return socket.isConnected();
+        }
+        return false;
+    }
+
+
+
+    public void sendData(String data) throws IOException {
+        Log.i("fuck ","fuck fuckfuckfuck大发送方式的发生的");
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Log.i("fuck ","fuck fuckfuckfuck");
+                OutputStream os = null;
+
+                try {
+                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                    byte data[]="abcd".getBytes();
+                    out.write(data);
+
+                    out.flush();
+
+
+                    Log.i("fuck ","fuck ");
+
+                } catch (IOException e) {
+
+                    Log.i("IOException","IOException",e);
+                }
+            }
+        }).start();
 
     }
 
